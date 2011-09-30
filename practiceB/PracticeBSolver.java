de5faac84d6dca09c73fe51bf033925f9236c4d3
Toshinari Nakamura <toshinarin@gmail.com>
2011-09-30 16:32:37 +0900

    modified test case parser.

--- practiceB/PracticeBSolver.java
+++ practiceB/PracticeBSolver.java
@@ -29,6 +29,14 @@ public class PracticeBSolver {
 		}
 	}
 
+	private static boolean canWrite(File file) {
+		if (file.exists() == false || file.isFile() == false
+				|| file.canWrite() == false) {
+			return false;
+		}
+		return true;
+	}
+
 	public static int openOutput(String fileName) {
 		File f = new File(fileName);
 		if (canWrite(f) == false) {
@@ -69,24 +77,10 @@ public class PracticeBSolver {
 		return 0;
 	}
 
-	public static String calc(long R, long k, int N, long[] groups) {
-		print("R={" + R + "}, k={" + k + "}, N={" + N + "}");
-		long i = 0, a = 0, c = 0, pax = 0, tmpPax = 0, total = 0;
-		int b = 0;
+	public static String calc(long A, long B, long P) {
+		print("A={" + A + "}, B={" + B + "}, P={" + P + "}");
 
-		for (a = 0; a < R; a++) {
-			pax = 0;
-			for (c = 0; c < N; c++) {
-				if ((tmpPax = pax + groups[b]) > k)
-					break;
-				pax = tmpPax;
-				b++;
-				if (b == N)
-					b = 0;
-			}
-			total += pax;
-		}
-		return "" + total;
+		return "0";
 	}
 
 	public static void exec(String fileName) {
@@ -111,35 +105,23 @@ public class PracticeBSolver {
 		int numCase = 0;
 		String ans = null;
 		String out = null;
-		long R = 0, k = 0;
-		int N = 0;
-		int i = 0, j = 0;
+		long A = 0, B = 0, P = 0;
+
+		int i = 0;
 		for (i = 0; i < mNumCase; i++) {
 			tmpStr = readLine();
 			if (tmpStr == null)
 				break;
 			strArray = tmpStr.split(" ");
-			if (strArray.length != 3) {
+			if (DEBUG && strArray.length != 3) {
 				print("failed to parse test case.");
 				return;
 			}
-			R = Long.parseLong(strArray[0]);
-			k = Long.parseLong(strArray[1]);
-			N = Integer.parseInt(strArray[2]);
+			A = Long.parseLong(strArray[0]);
+			B = Long.parseLong(strArray[1]);
+			P = Long.parseLong(strArray[2]);
 
-			tmpStr = readLine();
-			if (tmpStr == null)
-				break;
-			strArray = tmpStr.split(" ");
-			if (strArray.length != N) {
-				print("failed to parse test case. Group total doesn't match.");
-				return;
-			}
-			long[] groups = new long[N];
-			for (j = 0; j < N; j++) {
-				groups[j] = Long.parseLong(strArray[j]);
-			}
-			ans = calc(R, k, N, groups);
+			ans = calc(A, B, P);
 
 			out = "Case #" + (numCase + 1) + ": " + ans;
 			print(out);
@@ -153,27 +135,20 @@ public class PracticeBSolver {
 		closeOutput();
 	}
 
-	private static boolean canWrite(File file) {
-		if (file.exists() == false || file.isFile() == false
-				|| file.canWrite() == false) {
-			return false;
-		}
-		return true;
-	}
-
 	public static void main(String[] args) {
+		print("########### Start!! ############");
 		String fileName = null;
 		try {
 			fileName = args[0];
 			print("Open file: {" + fileName + "}");
 			exec(fileName);
 		} catch (Exception e) {
-			print("failed to execute. {" + e + "}");
+			print("failed to execute. error: {" + e + "}");
 		}
 		if (fileName == null) {
 			print("File name must be specified.");
 			return;
 		}
-		print("Solved!!");
+		print("########## Finished!! ##########");
 	}
 }
