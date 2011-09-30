4cff0375a454cda5fed093427cba1d2bc65d5686
Toshinari Nakamura <toshinarin@gmail.com>
2011-09-30 19:14:59 +0900

    speed up the calculation of prime numbers.

--- practiceB/PrimeNumbers.java
+++ practiceB/PrimeNumbers.java
@@ -1,7 +1,7 @@
 
 public class PrimeNumbers {
 	private static final boolean DEBUG = true;
-	private static final int LIST_LENGTH = 10000;
+	private static final int LIST_LENGTH = 1000000;
 
 	public static void print(String s) {
 		if (DEBUG == false)
@@ -9,25 +9,20 @@ public class PrimeNumbers {
 		System.out.println(s);
 	}
 	
-	public static long[] getPrimeNumbers(long num) {
-		long[] ret = new long[LIST_LENGTH];
+	public static int[] getPrimeNumbers() {
+		boolean[] flags = new boolean[LIST_LENGTH];
+		int[] ret = new int[LIST_LENGTH];
 
-		long a = 0;
-		int i = 0;
-		boolean isPrime = true;
-		for (a = 2; a < num; a++) {
-			isPrime = true;
-			for (i = 0; i < LIST_LENGTH && ret[i] != 0; i++) {
-				if (a % ret[i] == 0) {
-					isPrime = false;
-					break;
-				}
+		int a = 0, i = 0, j = 0;
+		for (a = 2; a < LIST_LENGTH; a++) {
+			if (flags[a] == true) {
+				continue;
 			}
-			if (i >= LIST_LENGTH) {
-				break;
-			}
-			if (isPrime) {
-				ret[i] = a;
+			ret[j] = a;
+			j++;
+
+			for (i = a; i < LIST_LENGTH; i += a) {
+				flags[i] = true;
 			}
 		}
 		return ret;
@@ -36,7 +31,7 @@ public class PrimeNumbers {
 	public static void main(String[] args) {
 		print("########### Start!! ############");
 		try {
-			long[] primeNumbers = getPrimeNumbers(1000000L);
+			int[] primeNumbers = getPrimeNumbers();
 			int i = 0;
 			for (i = 0; i < LIST_LENGTH && primeNumbers[i] != 0; i++) {
 				print(primeNumbers[i] + "");
